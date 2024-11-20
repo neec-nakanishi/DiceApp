0bc6119676934a7688d6f02448d9b4e0e3219a15
neec-nakanishi <nakanishi@g.neec.ac.jp>
2024-11-20 17:10:03 +0900

    Modified：結果表示

--- Chinchiro.java
+++ Chinchiro.java
@@ -41,13 +41,13 @@ public class Chinchiro {
 
         // 結果を表示
         if (oyaResult==koResult) {
-            System.out.println("引き分け");
+            System.out.println("【引き分け】");
         }
         else if (oyaResult<koResult) {
-            System.out.println("親の勝ち");
+            System.out.println("【親の勝ち】");
         }
         else {
-            System.out.println("子の勝ち");
+            System.out.println("【子の勝ち】");
         }
     }
 
